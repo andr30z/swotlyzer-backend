385afa6d76f325cd5366ce09574ad394507ca7ee
andr30z <andrelp1015@gmail.com>
2022-06-25 23:59:00 -0300

    feat: add spring eureka client and server

--- api-core/src/main/java/com/microservices/swotlyzer/api/core/SWOTLyzerApiApplication.java
+++ api-core/src/main/java/com/microservices/swotlyzer/api/core/SWOTLyzerApiApplication.java
@@ -2,6 +2,7 @@ package com.microservices.swotlyzer.api.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,9 +10,10 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableMongoAuditing
 @EnableWebMvc
 @SpringBootApplication
+@EnableEurekaClient
 @ComponentScan(basePackages = "com.microservices.swotlyzer")
 public class SWOTLyzerApiApplication {
-    
+
     public static void main(String[] args) {
         SpringApplication.run(SWOTLyzerApiApplication.class, args);
     }
--- mail-sender/src/main/java/com/microservices/swotlyzer/mail/sender/MailSenderApplication.java
+++ mail-sender/src/main/java/com/microservices/swotlyzer/mail/sender/MailSenderApplication.java
@@ -2,8 +2,10 @@ package com.microservices.swotlyzer.mail.sender;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MailSenderApplication {
 
     public static void main(String[] args) {
